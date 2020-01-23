d793367aedf090d072d21e54575af2c3806b622e
Jon Bash <jonbash@protonmail.com>
2020-01-23 09:11:18 -0800

    init pointers project

---
+++ Pointers/Pointers/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Pointers
+//
+//  Created by Jon Bash on 2020-01-23.
+//  Copyright © 2020 Jon Bash. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
